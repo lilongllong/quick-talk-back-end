5993fce02855ad29638f45cefafc2dd0623c8f2a
lilonglong <2453085960@qq.com>
2016-11-06 00:44:35 +0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+const server = require("http").Server(app);
+const SocketServer = require("./lib/MessageInterface");
+SocketServer.bindSocketIO(server);
+
+server.listen(8080);
