74c787c0c0466362c2bf87e6d8d453f892f7bb30
DengrongGuan12 <1031212913@qq.com>
2016-11-08 01:14:04 +0800

    complete

--- test/test.js
+++ test/test.js
@@ -1,14 +1,19 @@
 const socket = io.connect('http://localhost:8080/mx-qq');
 
 $(document).ready(() => {
-    socket.emit("post-message", {time: "2016 test", id: "test", message: "it is  a test", location: "Nanjing", distance: 200});
-    socket.emit("get-message", "Nanjing");
-    socket.on("message result", (data) => {
-        console.log("接受到消息结果");
-        console.log(data);
-    });
-    socket.emit("callback test", "callback message" , (data) => {
-
-        console.log("接受callback测试数据: " + data);
+    // socket.emit("post-message", {time: "2016 test", id: "test", message: "it is  a test", location: "Nanjing", distance: 200});
+    // socket.emit("get-message", "Nanjing");
+    // socket.on("message result", (data) => {
+    //     console.log("接受到消息结果");
+    //     console.log(data);
+    // });
+    // socket.emit("callback test", "callback message" , (data) => {
+    //
+    //     console.log("接受callback测试数据: " + data);
+    // });
+    socket.emit("change distance",10);
+    socket.emit("change location",{lat:112.02345,lng:30.8764});
+    socket.on("update message",(data) => {
+      console.log(data);
     });
 });
